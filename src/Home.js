db4b54318330133f24e460c8e73a07d9fb155ca7
pirabaasegar <61627407+pirabaasegar@users.noreply.github.com>
2024-09-18 22:25:59 -0400

    ---

--- src/Home.js
+++ src/Home.js
@@ -89,7 +89,7 @@ function App() {
           </div>
         </div>
       </section>
-      <section id="work" className="work overflow-hidden">
+      <section id="work" className="work overflow-hidden pt-5 mt-4">
         <div className="section-title text-center pb-0" data-aos="fade-up">
           <h2 className='fw-bold position-relative'>My work and projects</h2>
           <p>
